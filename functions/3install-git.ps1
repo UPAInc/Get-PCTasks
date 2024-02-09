9c5c257f5706d4e1dceb12a2ee5ef876c3b0e8d5
Eric Duncan <109294318+EricBDuncan@users.noreply.github.com>
2024-02-09 11:38:53 -0500

    Update 3install-git.ps1

--- functions/3install-git.ps1
+++ functions/3install-git.ps1
@@ -1,7 +1,8 @@
 $script:name=($MyInvocation.MyCommand.Name).Trim('.ps1')
 $gt=get-command git
+$wg=get-command winget
 if (!($gt)) {
-	if (winget) {
+	if ($wg) {
 			winget install --id Git.Git -e --source winget --scope machine --accept-package-agreements -h
 		} ELSE {
 			choco install -y git
