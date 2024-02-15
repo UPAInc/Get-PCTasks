e05e4ccb36cd00a4372788d113e85460a4baae0d
Eric Duncan <109294318+EricBDuncan@users.noreply.github.com>
2024-02-15 14:09:06 -0500

    1.0

--- functions/4install-BurntToast.ps1
+++ functions/4install-BurntToast.ps1
@@ -1,11 +1,12 @@
 $script:name=($MyInvocation.MyCommand.Name).Trim('.ps1')
 
 $chkbt=get-command New-BurntToastNotification -ErrorAction SilentlyContinue
-$wg=get-command winget -ErrorAction SilentlyContinue
+$wg=get-command choco -ErrorAction SilentlyContinue
 
 if (!($chkbt)) {
 	if ($wg) {
-			winget install --id BurntToast -e --source winget --scope machine --accept-package-agreements -h
+			#winget install --id BurntToast -e --source winget --scope machine --accept-package-agreements -h
+			choco install -y BurntToast-psmodule
 		} ELSE {
 			Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Confirm:$false -Scope AllUsers
 			install-module BurntToast -force -Confirm:$false -Scope AllUsers
