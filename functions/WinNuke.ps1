701b89cd56490d239b5cf5161eb909ecc2e0c3e8
Eric Duncan <109294318+EricBDuncan@users.noreply.github.com>
2024-02-22 17:37:37 -0500

    1.2
    Added confirm action for safety.

--- functions/WinNuke.ps1
+++ functions/WinNuke.ps1
@@ -1,18 +1,23 @@
+<#
+.VERSION 1.2
+#>
+
 $script:name=($MyInvocation.MyCommand.Name).Trim('.ps1')
 
-function WinNuke() {
+function WinNuke($options,$action) {
 	#Reset Windows
+	#$methodName = "doWipeMethod" #can be canceled by the user
+	#$methodName = "doWipeProtectedMethod" #possibly unbootable
+	if ($options -eq 'safe') {$methodName = "doWipeMethod"} ELSE {$methodName = "doWipeProtectedMethod"}
 	$namespaceName = "root\cimv2\mdm\dmmap"
 	$className = "MDM_RemoteWipe"
-	#$methodName = "doWipeMethod" #can be canceled by the user
-	$methodName = "doWipeProtectedMethod" #possibly unbootable
 	$session = New-CimSession
 	$params = New-Object Microsoft.Management.Infrastructure.CimMethodParametersCollection
 	$param = [Microsoft.Management.Infrastructure.CimMethodParameter]::Create("param", "", "String", "In")
 	$params.Add($param)
 	$instance = Get-CimInstance -Namespace $namespaceName -ClassName $className -Filter "ParentID='./Vendor/MSFT' and InstanceID='RemoteWipe'"
-	$session.InvokeMethod($namespaceName, $instance, $methodName, $params)
+	if ($action -eq 'Confirm') {"WinNuke invoked..."; $session.InvokeMethod($namespaceName, $instance, $methodName, $params)} ELSE {$Alert="WinNuke action was not confirmed!"}
+	if ($alert) {return $alert} ELSE {return "WinNuke Confirmed"}
 }
-
 
 write-host "$name loaded..." -ForegroundColor yellow -BackgroundColor black
