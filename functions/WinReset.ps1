d3d1771d522821700ed5d290c8c0da18ca26dfa6
Eric Duncan <109294318+EricBDuncan@users.noreply.github.com>
2024-02-23 16:01:22 -0500

    Update WinReset.ps1

--- functions/WinReset.ps1
+++ functions/WinReset.ps1
@@ -16,19 +16,20 @@ function WinReset($options,$action) {
         #$methodName = "doWipeProtectedMethod" #possibly unbootable
         if ($options -eq 'safe') {$methodName = "doWipeMethod"} ELSE {$methodName = "doWipeProtectedMethod"}
         $namespaceName = "root\cimv2\mdm\dmmap"
-        $className = "MDM_RemoteWipe"
-        $session = New-CimSession
-        $params = New-Object Microsoft.Management.Infrastructure.CimMethodParametersCollection
-        $param = [Microsoft.Management.Infrastructure.CimMethodParameter]::Create("param", "", "String", "In")
-        $params.Add($param)
-        $instance = Get-CimInstance -Namespace $namespaceName -ClassName $className -Filter "ParentID='./Vendor/MSFT' and InstanceID='RemoteWipe'"
-        
-        $session.InvokeMethod($namespaceName, $instance, $methodName, $params)
-    }
-    
+	$className = "MDM_RemoteWipe"
+	#$methodName = "doWipeMethod" #can be canceled by the user
+	$methodName = "doWipeProtectedMethod" #possibly unbootable
+	$session = New-CimSession
+	$params = New-Object Microsoft.Management.Infrastructure.CimMethodParametersCollection
+	$param = [Microsoft.Management.Infrastructure.CimMethodParameter]::Create("param", "", "String", "In")
+	$params.Add($param)
+	$instance = Get-CimInstance -Namespace $namespaceName -ClassName $className -Filter "ParentID='./Vendor/MSFT' and InstanceID='RemoteWipe'"
+	$session.InvokeMethod($namespaceName, $instance, $methodName, $params)
+    return
+    } #WRSB
     Start-Job -ScriptBlock $WRSB -ArgumentList $options,$action
     Get-Job
-}  ELSE {"WinReset action was not confirmed!"}
+    }  ELSE {"WinReset action was not confirmed!"}
 return 
 }
 
